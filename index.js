7ebc55fe6ebf65ee7a9d13936128fe710461a4ce
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-12-30 02:21:04 +0200

    created constructor that accepts an index, timestamp and a hash

---
+++ index.js
@@ -0,0 +1,14 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+    }
+
+    calculateHash() {
+        return SHA256( this.index + this.timestamp + JSON.stringify(this.data) ).toString();
+    }
+}
