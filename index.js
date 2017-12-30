60c0cf129d5a9230bc35d5bd9485614036cb07c3
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-12-30 02:40:48 +0200

    created Blockchain CLASS with constructor to construct the chain, created the genesis block, created methods to get latest block, add new blocks, and newed up an instance of a mannyCoin and logged it out to console.

--- index.js
+++ index.js
@@ -12,3 +12,35 @@ class Block {
         return SHA256( this.index + this.timestamp + JSON.stringify(this.data) ).toString();
     }
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '28/12/2017','Genesis Block', '0');
+    }
+
+    getLatestBlock() {
+        // returns the latest block in the chain
+        return this.chain[this.chain.length - 1];
+    }
+
+    // adding new block onto the chain
+    addBlock(newBlock) {
+        // set the previouse hash property
+        newBlock.previousHash = this.getLatestBlock().hash;
+        //recalculate its hash
+        newBlock.hash = newBlock.calculateHash();
+        // push it onto the chain
+        this.chain.push(newBlock);
+    }
+}
+
+// create an instance of the Blockchain
+let mannyCoin = new Blockchain();
+mannyCoin.addBlock(new Block(1, '28/12/2017', { amount: 5 }));
+mannyCoin.addBlock(new Block(2, '29/12/2017', { amount: 20 }));
+
+console.log(JSON.stringify(mannyCoin, null, 4));
